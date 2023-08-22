1c7c29c8cb76dcc112ae3d07c0313cab405fe92c
Ikhlas Qassimi <129891260+ikhlas1936@users.noreply.github.com>
2023-08-22 23:26:59 +0100

    Delete front end

--- HotelFront/src/parts/IconText.jsx
+++
@@ -1,9 +0,0 @@
-import Button from "../elements/Button";
-
-export default function IconText() {
-  return (
-    <Button className="brand-text-icon" href="" type="link">
-      Travel<span className="text-gray-900">Tribe.</span>
-    </Button>
-  );
-}
