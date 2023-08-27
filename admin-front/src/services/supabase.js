890bb4e7221ba427efa17a797632924e4121536c
Ikhlas Qassimi <129891260+ikhlas1936@users.noreply.github.com>
2023-08-27 18:11:08 +0100

    Delete admin-front directory

--- admin-front/src/services/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabaseUrl = "https://cvjeprznvbxblcnothcl.supabase.co";
-
-const supabaseKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2amVwcnpudmJ4Ymxjbm90aGNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg2NTE4MDEsImV4cCI6MjAwNDIyNzgwMX0.C-wCmtYAYuAyoFf29tlOJ1GQbaiaDVxJpeXHJBvXGn8 ";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
